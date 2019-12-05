e387b8e428c501c84ddb128e609a0f8b0dce472f
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-12-04 17:39:06 -0800

    Update execute.sh to correct TAU env settings

--- examples/execute.sh
+++ examples/execute.sh
@@ -5,12 +5,18 @@
 rm -rf DISABLED* ENABLED* \
     timemory-enabled-output
 
+export TAU_THROTTLE=0
+export TAU_TRACE=0
+export TAU_SUMMARY=0
+export TAU_SET_NODE=0
+
 #----------------------------------------------------------#
 #       Disabled
 #----------------------------------------------------------#
 unset CALI_CONFIG_PROFILE
-export TIMEMORY_ENABLED=OFF
 export TAU_PROFILE=0
+export TIMEMORY_ENABLED=OFF
+export TIMEMORY_OUTPUT_PATH=timemory-disabled-output
 
 set -e
 python ./execute.py -p DISABLED ${ARGS} $@
@@ -20,8 +26,8 @@ set +e
 #       Enabled
 #----------------------------------------------------------#
 export CALI_CONFIG_PROFILE=runtime-report
-export TIMEMORY_ENABLED=ON
 export TAU_PROFILE=1
+export TIMEMORY_ENABLED=ON
 export TIMEMORY_OUTPUT_PATH=timemory-enabled-output
 
 set -e
